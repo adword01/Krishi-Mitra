3ecff7f549d999c4871ae6086b73afa00f4a16a7
kanishkgangwar <gangwarkanisk@gmail.com>
2023-03-22 16:29:51 +0530

    mac push

--- app/src/main/java/com/example/krishimitra/fragments/GreetFragment.kt
+++ app/src/main/java/com/example/krishimitra/fragments/GreetFragment.kt
@@ -5,56 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.krishimitra.R
+import com.example.krishimitra.databinding.FragmentGreetBinding
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GreetFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GreetFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : FragmentGreetBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentGreetBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_greet, container, false)
+        val date = Date()
+        val cal: Calendar = Calendar.getInstance()
+        cal.time = date
+        when(cal.get(Calendar.HOUR_OF_DAY)){
+            in 0..12 ->{
+                binding.greettext.text = "Good Morning!!"
+            }
+            in 12..17 ->{
+                binding.greettext.text = "Good Afternoon!!"
+            }
+            in 17..21 ->{
+                binding.greettext.text = "Good evening!!"
+            }
+            else -> {
+                binding.greettext.text = "Good Night!!"
+            }
+        }
+
+        //    return inflater.inflate(R.layout.fragment_greet, container, false)
+
+        return binding.root
+
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GreetFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GreetFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
