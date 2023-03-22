28d67ca7d4d8ea8504abac75da018d4d614df3ea
Aditya Kumar <aditya.ak4747@gmail.com>
2023-03-22 13:43:04 +0530

    adding test button

--- app/src/main/java/com/example/krishimitra/LoginActivity.kt
+++ app/src/main/java/com/example/krishimitra/LoginActivity.kt
@@ -2,17 +2,25 @@ package com.example.krishimitra
 
 import android.app.Dialog
 import android.app.ProgressDialog.show
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.airbnb.lottie.LottieAnimationView
+import com.example.krishimitra.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityLoginBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+//        setContentView(R.layout.activity_login)
+
+        var binding: ActivityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
 //        val btn = findViewById<Button>(R.id.button)
@@ -27,6 +35,15 @@ class LoginActivity : AppCompatActivity() {
 
         val animationView = dialog.findViewById<LottieAnimationView>(R.id.animationView)
         animationView.playAnimation()
+
+//        animationView.setOnFocusChangeListener{
+//            val intent = Intent(this,MainActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        val login_btn = findViewById<Button>(R.id.closeButton)
+
+
 
         val closeButton = dialog.findViewById<Button>(R.id.closeButton)
         closeButton.setOnClickListener {
