5d911a0d5113bde59b2a66772ba0ac36483061df
kanishkgangwar <gangwarkanisk@gmail.com>
2023-03-23 13:03:30 +0530

    mac push

--- app/src/main/java/com/example/krishimitra/LoginActivity.kt
+++ app/src/main/java/com/example/krishimitra/LoginActivity.kt
@@ -1,18 +1,23 @@
 package com.example.krishimitra
 
+import android.app.Activity
 import android.app.Dialog
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
 import com.example.krishimitra.databinding.ActivityLoginBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
@@ -28,6 +33,10 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var gso: GoogleSignInOptions
     private lateinit var gsc : GoogleSignInClient
 
+    companion object {
+        private const val RC_SIGN_IN = 123
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,24 +47,28 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+
         auth = FirebaseAuth.getInstance()
         currentuser = auth.currentUser
         auth = FirebaseAuth.getInstance()
 
 
-        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
+
         gsc = GoogleSignIn.getClient(this, gso)
 
-        val acct = GoogleSignIn.getLastSignedInAccount(this)
-        if (acct != null) {
-            navigateToSecondActivity()
-        }
 
 
         binding.signIn.setOnClickListener {
-            signIn()
+            //signIn()
+            signInGoogle()
+//            val signInIntent = mGoogleSignInClient.signInIntent
+//            startActivityForResult(signInIntent, RC_SIGN_IN)
+
         }
         binding.loginButton.setOnClickListener {
           //  binding.verifyBtn.setVisibility(View.VISIBLE)
@@ -64,8 +77,7 @@ class LoginActivity : AppCompatActivity() {
 
             show()
         }
-
-//        binding.verifyBtn.setOnClickListener {
+    //        binding.verifyBtn.setOnClickListener {
 //            val intent =Intent(this,HomeActivity::class.java)
 //            startActivity(intent)
 //        }
@@ -78,76 +90,48 @@ class LoginActivity : AppCompatActivity() {
 //        show()
     }
 
-
-    //what is this going on
-    private fun signIn() {
+    private fun signInGoogle(){
         val signInIntent = gsc.signInIntent
-        startActivityForResult(signInIntent, 1000)
+        launcher.launch(signInIntent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode == Activity.RESULT_OK){
 
-        if (requestCode == 1000) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-
-            try {
-                task.getResult(ApiException::class.java)
-                navigateToSecondActivity()
-            } catch (e: ApiException) {
-                Log.d("Error",e.toString())
-                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT).show()
-            }
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
         }
     }
-    private fun navigateToSecondActivity() {
-        finish()
-        val intent = Intent(this, HomeActivity::class.java)
-        startActivity(intent)
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
+        }
     }
 
 
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val intent = Intent(this, HomeActivity::class.java)
+                intent.putExtra("email", account.email)
+                intent.putExtra("name", account.displayName)
+                startActivity(intent)
+                Log.d("Success","Successful")
+            } else {
+                Log.d("Error",it.exception.toString())
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
 
-//    private fun signInGoogle(){
-//        val signInIntent = googleSignInClient.signInIntent
-//        launcher.launch(signInIntent)
-//    }
-//
-//    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-//            result ->
-//        if (result.resultCode == Activity.RESULT_OK){
-//
-//            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-//            handleResults(task)
-//        }
-//    }
-//    private fun handleResults(task: Task<GoogleSignInAccount>) {
-//        if (task.isSuccessful){
-//            val account : GoogleSignInAccount? = task.result
-//            if (account != null){
-//                updateUI(account)
-//            }
-//        }else{
-//            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
-//        }
-//    }
-//
-//
-//    private fun updateUI(account: GoogleSignInAccount) {
-//        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-//        auth.signInWithCredential(credential).addOnCompleteListener {
-//            if (it.isSuccessful) {
-//                val intent = Intent(this, HomeActivity::class.java)
-//                intent.putExtra("email", account.email)
-//                intent.putExtra("name", account.displayName)
-//                startActivity(intent)
-//            } else {
-//                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-//
-//            }
-//        }
-//
-//    }
+            }
+        }
+
+    }
 
     private fun show(){
         val dialog = Dialog(this)
--- app/src/main/java/com/example/krishimitra/fragments/UserProfileFragment.kt
+++ app/src/main/java/com/example/krishimitra/fragments/UserProfileFragment.kt
@@ -1,53 +1,109 @@
 package com.example.krishimitra.fragments
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.example.krishimitra.EditProfileActivity
+import com.example.krishimitra.LoginActivity
 import com.example.krishimitra.R
 import com.example.krishimitra.databinding.FragmentGreetBinding
 import com.example.krishimitra.databinding.FragmentUserProfileBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class UserProfileFragment : Fragment() {
 
 
     private lateinit var Editbutton : Button
     private lateinit var binding : FragmentUserProfileBinding
+    private lateinit var auth : FirebaseAuth
+    private lateinit var authName : String
+    private lateinit var authEmail : String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+
         binding = FragmentUserProfileBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
        // return inflater.inflate(R.layout.fragment_user_profile, container, false)
 
+        auth = FirebaseAuth.getInstance()
+        authName = auth.currentUser!!.displayName.toString()
+        authEmail = auth.currentUser!!.email.toString()
+
+        binding.profileName.text = authName
+        binding.mobNumber.text =  authEmail
 
         getData()
+        binding.logoutbtn.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val gsc = GoogleSignIn.getClient(requireActivity(),gso)
+            gsc.signOut()
+            val intent = Intent(activity,LoginActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+           // auth.signOut()
+        }
         binding.editprofilebtn.setOnClickListener {
            val intent = Intent(activity,EditProfileActivity::class.java)
             startActivity(intent)
+
+
         }
         return binding.root
     }
 
     private fun getData() {
-        val db = FirebaseDatabase.getInstance().reference
+    //    val db = FirebaseDatabase.getInstance().reference
 
-        db.child("User").get().addOnSuccessListener {
-            if(it.exists()){
-                binding.Nametxt.text = it.child("name").value.toString()
-                binding.emailtxt.text = it.child("email").value.toString()
-                binding.phonetxt.text = it.child("mobileNumber").value.toString()
-                binding.addresstxt.text = it.child("location").value.toString()
+        val db = Firebase.firestore
+        val docRef = db.collection("User").document(authEmail)
+
+        docRef.get()
+            .addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val email = documentSnapshot.getString("email")
+                    val location = documentSnapshot.getString("location")
+                    val mobNumber = documentSnapshot.getString("mobileNumber")
+                    val name = documentSnapshot.getString("name")
+                    binding.Nametxt.text = name
+                    binding.emailtxt.text = email
+                    binding.phonetxt.text = mobNumber
+                    binding.addresstxt.text = location
+
+                    Log.d(TAG, "Email: $email")
+                } else {
+                    Log.d(TAG, "Document does not exist")
+                }
             }
-        }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting document", exception)
+            }
+
+//        db.child("User").get().addOnSuccessListener {
+//            if(it.exists()){
+//                binding.Nametxt.text = it.child("name").value.toString()
+//                binding.emailtxt.text = it.child("email").value.toString()
+//                binding.phonetxt.text = it.child("mobileNumber").value.toString()
+//                binding.addresstxt.text = it.child("location").value.toString()
+//            }
+//        }
     }
 
 
