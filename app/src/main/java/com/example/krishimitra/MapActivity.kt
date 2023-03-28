babfc64b7332a75c88ea40dbc9e7db15001c1995
Aditya Kumar <aditya.ak4747@gmail.com>
2023-03-28 13:38:28 +0530

    Aditya

---
+++ app/src/main/java/com/example/krishimitra/MapActivity.kt
@@ -0,0 +1,33 @@
+package com.example.krishimitra
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+// Implement OnMapReadyCallback.
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Set the layout file as the content view.
+        setContentView(R.layout.activity_map)
+
+        // Get a handle to the fragment and register the callback.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    // Get a handle to the GoogleMap object and display marker.
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
+    }
+}
