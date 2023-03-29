619b07aca8a3b0b15327a7f9903a13e37f18b188
Aditya Kumar <aditya.ak4747@gmail.com>
2023-03-29 16:06:39 +0530

    Aditya

--- app/src/main/java/com/example/krishimitra/MapActivity.kt
+++ app/src/main/java/com/example/krishimitra/MapActivity.kt
@@ -1,16 +1,35 @@
 package com.example.krishimitra
 
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationRequest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 
 // Implement OnMapReadyCallback.
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
+
+    private var mMap: GoogleMap? = null
+    internal lateinit var mLastLocation: Location
+    internal lateinit var mLocationResult: LocationRequest
+    internal lateinit var mLocationCallback: LocationCallback
+    internal var mCurrLocationMarker: Marker? = null
+    internal var mGoogleApiClient: GoogleApiClient? = null
+    internal lateinit var mLocationRequest: LocationRequest
+    internal var mFusedLocationClient: FusedLocationProviderClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Set the layout file as the content view.
@@ -29,5 +48,28 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 .position(LatLng(0.0, 0.0))
                 .title("Marker")
         )
+    }
+
+    override fun onLocationChanged(location: Location) {
+        mLastLocation = location
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker!!.remove()
+        }
+        //Place current location marker
+        val latLng = LatLng(location.latitude, location.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(latLng)
+        markerOptions.title("Current Position")
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+        mCurrLocationMarker = mMap!!.addMarker(markerOptions)
+
+        //move map camera
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap!!.animateCamera(CameraUpdateFactory.zoomTo(11f))
+
+        //stop location updates
+        if (mGoogleApiClient != null) {
+            mFusedLocationClient?.removeLocationUpdates(mLocationCallback)
+        }
     }
 }
