75f0ffee56aed4fb4e74f749c7ddb7d6e17eb2e4
Aditya Kumar <aditya.ak4747@gmail.com>
2023-04-16 22:24:07 +0530

    Aditya

---
+++ app/src/main/java/com/example/krishimitra/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.krishimitra
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+
+    }
+}
